0505f5958e9a1b13bdcb59677ae1bee23efc332a
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-02-20 20:19:51 -0500

    :sparkles: Setup boilerplate for next.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Home = props => (
+  <div>
+    <p>Hey!</p>
+    <Link href="/sell">Sell!</Link>
+  </div>
+)
+
+export default Home
