7bbf3e5d64b361d3f685bb2729bed9894a270073
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-02-21 20:57:53 -0500

    :lipstick: Setup theme provider

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Meta from './Meta'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightGrey: '#e1e1e1',
+  offWhite: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px',
+  bp: '1300px'
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.black};
+  padding: 2rem;
+`
+
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
