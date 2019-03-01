88bea810f38e545749df1671416eb93995f9621f
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-02-28 23:25:36 -0500

    :sparkles: Setup GraphQL Yoga server!

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+require('dotenv').config({ path: 'settings.dev.env' })
+
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO: Use express middleware to handle cookies (JWT)
+// TODO: Use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    },
+    port: process.env.PORT
+  },
+  ({ port }) => {
+    console.log(`\nServer is now running at: \nhttp://localhost:${port}`)
+  }
+)
