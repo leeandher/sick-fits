f5bf1baac60ba35d0b530f5c2f0bb624a6086d42
Leander Rodrigues <me@leander.xyz>
2019-06-18 09:57:29 -0400

    ✨ Add resolver for pagination

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require('prisma-binding')
 
 const Query = {
   items: forwardTo('db'),
-  item: forwardTo('db')
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db')
   /*
   
   Since the below function operates exactly the same as on the generated schema,
