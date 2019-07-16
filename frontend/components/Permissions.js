59639ab319fc7518c7abb73bfc6174130f7772a1
Leander Rodrigues <me@leander.xyz>
2019-07-16 18:13:45 -0400

    ✨ Implement permissions UI

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 
 import ErrorMessage from './ErrorMessage'
 import Table from './styles/Table'
+import SickButton from './styles/SickButton'
 
 const ALL_USERS_AND_PERMISSIONS_QUERY = gql`
   query ALL_USERS_AND_PERMISSIONS_QUERY {
@@ -24,8 +25,7 @@ const ALL_USERS_AND_PERMISSIONS_QUERY = gql`
 const Permissions = props => (
   <Query query={ALL_USERS_AND_PERMISSIONS_QUERY}>
     {({ data, loading, error }) => {
-      const permissionValues = data.__type.enumValues.map(({ name }) => name)
-      console.log(permissionValues)
+      const allPermissions = data.__type.enumValues.map(({ name }) => name)
       return (
         <>
           <ErrorMessage error={error} />
@@ -36,9 +36,21 @@ const Permissions = props => (
                 <tr>
                   <th>Name</th>
                   <th>Email</th>
+                  {allPermissions.map(permission => (
+                    <th key={permission}>{permission}</th>
+                  ))}
+                  <th>👇</th>
                 </tr>
               </thead>
-              <tbody>test</tbody>
+              <tbody>
+                {data.users.map(user => (
+                  <User
+                    user={user}
+                    allPermissions={allPermissions}
+                    key={user.id}
+                  />
+                ))}
+              </tbody>
             </Table>
           </div>
         </>
@@ -46,6 +58,33 @@ const Permissions = props => (
     }}
   </Query>
 )
+
+class User extends React.Component {
+  render() {
+    const { user, allPermissions } = this.props
+    const { name, email, permissions } = user
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{email}</td>
+        {allPermissions.map(permission => (
+          <td key={permission}>
+            <label htmlFor={`${user.id}-permission-${permission}`}>
+              <input
+                type="checkbox"
+                name={`${user.id}-permission-${permission}`}
+                checked={permissions.includes(permission)}
+              />
+            </label>
+          </td>
+        ))}
+        <td>
+          <SickButton>Update</SickButton>
+        </td>
+      </tr>
+    )
+  }
+}
 
 export default Permissions
 export { ALL_USERS_AND_PERMISSIONS_QUERY }
