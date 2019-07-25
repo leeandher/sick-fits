fabb3c01086232405f4b60dd9546cfc7cceb4e16
Leander Rodrigues <me@leander.xyz>
2019-07-25 14:36:30 -0400

    ✨ Add UI for adding cart item

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 import { Mutation } from "react-apollo"
 import gql from "graphql-tag"
 
+import { CURRENT_USER_QUERY } from "./User"
+
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
     addToCart(id: $id) {
@@ -15,9 +17,15 @@ class AddToCart extends Component {
   render() {
     const { id, children } = this.props
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-        {(addToCart, { data }) => (
-          <button onClick={addToCart}>{children}</button>
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{ id }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(addToCart, { loading }) => (
+          <button onClick={addToCart} disabled={loading}>
+            Add{loading && "ing"} to 🛒
+          </button>
         )}
       </Mutation>
     )
