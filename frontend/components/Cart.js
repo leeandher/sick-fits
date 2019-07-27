c189d8dd40cb7919985c0bbbe222810056622aa1
Leander Rodrigues <me@leander.xyz>
2019-07-27 15:49:44 -0400

    ♻️ Clean up code with composition

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Query, Mutation } from "react-apollo"
 import gql from "graphql-tag"
+import { adopt } from "react-adopt"
 
 import User from "./User"
 import CartItem from "./CartItem"
@@ -24,47 +25,50 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `
 
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+})
+
 const Cart = () => {
   return (
-    <User>
-      {({ data: { me } }) =>
-        !me ? null : (
-          <Mutation mutation={TOGGLE_CART_MUTATION}>
-            {toggleCart => (
-              <Query query={LOCAL_STATE_QUERY}>
-                {({ data }) => (
-                  <CartStyles open={data.cartOpen}>
-                    <header>
-                      <CloseButton title="close" onClick={toggleCart}>
-                        &times;
-                      </CloseButton>
-                      <Supreme>
-                        {`${me.name}${me.name.endsWith("s") ? "'" : "'s"} `}🛒
-                      </Supreme>
-                      <p>
-                        You have {me.cart.length} item
-                        {me.cart.length !== 1 && "s"} in your cart.
-                      </p>
-                    </header>
-                    <ul>
-                      {me.cart.map(cartItem => (
-                        <CartItem key={cartItem.id} cartItem={cartItem}>
-                          {cartItem.id}
-                        </CartItem>
-                      ))}
-                    </ul>
-                    <footer>
-                      <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                      <SickButton>Checkout</SickButton>
-                    </footer>
-                  </CartStyles>
-                )}
-              </Query>
-            )}
-          </Mutation>
+    <Composed>
+      {({ user, toggleCart, localState }) => {
+        const { me } = user.data
+        const { cartOpen } = localState.data
+        if (!me) return null
+        return (
+          <CartStyles open={cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>
+                &times;
+              </CloseButton>
+              <Supreme>
+                {`${me.name}${me.name.endsWith("s") ? "'" : "'s"} `}🛒
+              </Supreme>
+              <p>
+                You have {me.cart.length} item
+                {me.cart.length !== 1 && "s"} in your cart.
+              </p>
+            </header>
+            <ul>
+              {me.cart.map(cartItem => (
+                <CartItem key={cartItem.id} cartItem={cartItem}>
+                  {cartItem.id}
+                </CartItem>
+              ))}
+            </ul>
+            <footer>
+              <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
         )
-      }
-    </User>
+      }}
+    </Composed>
   )
 }
 
---
+++ notes/6 - Advanced UI and Code Quality.md
@@ -0,0 +1,18 @@
+# Advanced React
+
+_Some helpful notes for developing modern applications in React. These may be a good reference!_
+
+---
+
+## Composition
+
+
+<!-- Fixing weird issues with  -->
+```js
+user: ({ render }) => <User>{render}</User>,
+```
+even though this works
+
+```js
+user: <User />,
+```
